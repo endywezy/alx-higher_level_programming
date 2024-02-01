756035eca9a975a832bf3fce8adb24753d274d37
endywezy <endywezy@gmail.com>
2024-01-31 18:04:12 -0800

    Everything is object

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-
 counter = 0
-def magic_string(): global counter; counter += 1; return "BestSchool" * counter
+def magic_string():
+    global counter; counter += 1; return "BestSchool" * counter
