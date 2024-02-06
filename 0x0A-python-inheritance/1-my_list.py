24a028abe0dd7706e1d4bad5d8d2a0883ce19892
endywezy <endywezy@gmail.com>
2024-02-05 18:14:22 -0800

    Python - Inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,5 +6,5 @@ class MyList(list):
     """Implements sorted printing for the built-in list class"""
 
     def print_sorted(self):
-        sorted_list = sorted(self)
+        """sorted_list = sorted(self)"""
         print(sorted_list)
