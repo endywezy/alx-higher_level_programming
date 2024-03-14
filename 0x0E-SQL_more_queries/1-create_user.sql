bc4255d95fecb07fb0ec76dc58733f993a8cc9fc
endywezy <endywezy@gmail.com>
2024-03-14 01:29:34 -0700

    SQL - More queries

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,9 +1,7 @@
--- This Creates the user user_0d_1 with all privileges.
+-- Creates the user user_0d_1 with all privileges.
 CREATE USER
     IF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
-   ON *.*
-   TO 'user_0d_1'@'localhost'
-   IDENTIFIED BY 'user_0d_1_pwd';
-FLUSH PRIVILEGES;
+	    IDENTIFIED BY 'user_0d_1_pwd';
+	GRANT ALL PRIVILEGES
+	   ON *.*
+	   TO 'user_0d_1'@'localhost';
