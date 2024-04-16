6df5a745abe4de992e7c8d9d2d5cc7f2dbebd78a
endywezy <endywezy@gmail.com>
2024-04-15 18:57:23 -0700

    Python MySQL Objects, Scopes and Closures

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -5,17 +5,33 @@ Prints the first State object from the database hbtn_0e_6_usa.
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from model_state import State, Base
 
 if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database name>".format(sys.argv[0]))
+        sys.exit(1)
+
+    username, password, database = sys.argv[1:]
+
+    # Connect to MySQL server
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+                          .format(username, password, database),
+                          pool_pre_ping=True)
+
+    # Create a session
     Session = sessionmaker(bind=engine)
     session = Session()
 
+    # Get the first state ordered by ID
     state = session.query(State).order_by(State.id).first()
+
+    # Print the state information
     if state is None:
         print("Nothing")
     else:
-        print("{}: {}".format(state.id, state.name)))
+        print("{}: {}".format(state.id, state.name))  # Corrected formatting
+
+    # Close the session
+    session.close()
+
