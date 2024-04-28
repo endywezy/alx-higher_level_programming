01a5667ba6779e41fa248e2040fa028b3cfc4ccd
endywezy <endywezy@gmail.com>
2024-04-28 04:35:30 -0700

    Python - Network #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
+"""Fetches https://alx-intranet.hbtn.io/status using requests."""
 import requests
 
-if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
+
+def main():
+  """Fetches the status and prints the response in a formatted way."""
+  url = "https://alx-intranet.hbtn.io/status"
+  try:
+    response = requests.get(url)
+    response.raise_for_status()  # Raise an exception for non-2xx status codes
+
     print("Body response:")
-    print(f"\t- type: {type(r.text)}")
-    print(f"\t- content: {r.text}")
+    print("\t- type:", type(response.text))
+    print("\t- content:", response.text)
+  except requests.exceptions.RequestException as e:
+    print("Error: {}".format(e))
+
+
+if __name__ == "__main__":
+  main()
