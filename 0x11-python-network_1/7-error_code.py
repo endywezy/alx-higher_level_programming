ffc43414df6f7784b35bf614631cdda1d59e88e7
endywezy <endywezy@gmail.com>
2024-04-28 04:32:47 -0700

    Python - Network #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-""" 
-A script that takes in a URL and sends a request to
-the URL and displays the body of the response while
-handling exceptions
+"""
+Python script that sends a request to the URL and
+displays:
+- The body of the response if there are no errors
+- The error code when there is an HTTP error.
 """
 import requests
 import sys
 
-if __name__ == "__main__":
-    url = sys.argv[1]
-    r = requests.get(url)
 
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
     if r.status_code >= 400:
         print("Error code: {}".format(r.status_code))
     else:
