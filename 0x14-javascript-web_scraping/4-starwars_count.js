3722acaa28a9fe90439454c19bf0186b502e4490
endywezy <endywezy@gmail.com>
2024-05-21 21:07:37 -0700

    JavaScript - Web scraping

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,26 +1,28 @@
 #!/usr/bin/node
 
 const request = require('request');
-const apiUrl = process.argv[2];
-const wedgeId = '18';
 
+const apiUrl = process.argv[2]; // Get the API URL from the command line argument
+const characterId = 18; // Character ID for Wedge Antilles
+
+// Make a request to the Star Wars API
 request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    return;
-  }
+    if (!error && response.statusCode == 200) {
+        const filmsData = JSON.parse(body).results;
+        let movieCount = 0;
 
-  const data = JSON.parse(body);
-  const films = data.results;
-  let count = 0;
+        // Loop through each movie data
+        filmsData.forEach((movie) => {
+            const characters = movie.characters.map((character) => character.split('/').slice(-2, -1)[0]);
+            
+            // Check if Wedge Antilles is present in the characters list
+            if (characters.includes(characterId.toString())) {
+                movieCount++;
+            }
+        });
 
-  films.forEach(film => {
-    film.characters.forEach(characterUrl => {
-      if (characterUrl.includes('/18/')) {
-        count++;
-      }
-    });
-  });
-
-  console.log(count);
+        console.log(movieCount);
+    } else {
+        console.log('Error fetching data from the API');
+    }
 });
