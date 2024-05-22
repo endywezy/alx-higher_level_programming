20078a47705369d84873b68e5e534af3b3c50970
endywezy <endywezy@gmail.com>
2024-05-21 21:50:48 -0700

    JavaScript - Web scraping

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
+
 const movieId = process.argv[2];
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
@@ -11,20 +12,39 @@ request(apiUrl, (error, response, body) => {
   }
 
   try {
-    const movie = JSON.parse(body);
-    const characters = movie.characters;
-    
-    characters.forEach(characterUrl => {
-      request(characterUrl, (charError, charResponse, charBody) => {
-        if (charError) {
-          console.error('Error:', charError);
-          return;
-        }
+    const movieData = JSON.parse(body);
+    const characterUrls = movieData.characters;
 
-        const character = JSON.parse(charBody);
-        console.log(character.name);
+    const fetchCharacterData = (url) => {
+      return new Promise((resolve, reject) => {
+        request(url, (charError, charResponse, charBody) => {
+          if (charError) {
+            reject(charError);
+          } else {
+            resolve(JSON.parse(charBody).name);
+          }
+        });
       });
-    });
+    };
+
+    const fetchAllCharacters = async () => {
+      const characters = [];
+      for (const characterUrl of characterUrls) {
+        const characterName = await fetchCharacterData(characterUrl);
+        characters.push(characterName);
+      }
+      return characters;
+    };
+
+    fetchAllCharacters()
+      .then(characters => {
+        characters.forEach(character => {
+          console.log(character);
+        });
+      })
+      .catch(err => {
+        console.error('Error fetching character data:', err);
+      });
   } catch (err) {
     console.error('Error parsing JSON:', err);
   }
